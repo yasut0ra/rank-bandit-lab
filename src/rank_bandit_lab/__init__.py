dbdf0e8269153aec7d1417a920ff338603a06e0a
yasut0ra <yastar.tkm83@gmail.com>
2025-11-11 04:17:10 +0900

    Enhance __init__.py by importing additional environment classes: DependentClickEnvironment and PositionBasedEnvironment, to support new ranking bandit simulation models.

--- src/rank_bandit_lab/__init__.py
+++ src/rank_bandit_lab/__init__.py
@@ -1,6 +1,10 @@
 """Ranking bandit experimentation helpers."""
 
-from .environment import CascadeEnvironment
+from .environment import (
+    CascadeEnvironment,
+    DependentClickEnvironment,
+    PositionBasedEnvironment,
+)
 from .policies import EpsilonGreedyRanking, RankingPolicy, ThompsonSamplingRanking
 from .simulator import BanditSimulator, SimulationLog
 from .types import Document, Interaction
@@ -8,9 +12,11 @@ from .types import Document, Interaction
 __all__ = [
     "BanditSimulator",
     "CascadeEnvironment",
+    "DependentClickEnvironment",
     "Document",
     "EpsilonGreedyRanking",
     "Interaction",
+    "PositionBasedEnvironment",
     "RankingPolicy",
     "SimulationLog",
     "ThompsonSamplingRanking",
